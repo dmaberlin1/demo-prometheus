18c5715380b2a8ab6f3df7d1c528f708e69b1e6d
dmaberlin1 <dmaberlin77@gmail.com>
2024-06-20 00:58:05 +0300

    feat: feat jdbc connection

--- src/main/java/com/dmadev/prometheus/controller/MetricsController.java
+++ src/main/java/com/dmadev/prometheus/controller/MetricsController.java
@@ -1,9 +1,8 @@
 package com.dmadev.prometheus.controller;
 
 import com.dmadev.prometheus.api.response.DatabaseMetricResult;
-import com.dmadev.prometheus.entity.Employee;
 import com.dmadev.prometheus.service.DefaultDatabaseMetricsService;
-import com.dmadev.prometheus.service.EmployeeService;
+import com.dmadev.prometheus.util.DatabaseCheckMetaData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,20 +18,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MetricsController {
 
- private final DefaultDatabaseMetricsService defaultDatabaseMetricsService;
+    private final DefaultDatabaseMetricsService defaultDatabaseMetricsService;
 
- @GetMapping("/query")
- public ResponseEntity<String> executeAndReturnQueryResults() {
-  try {
-   List<DatabaseMetricResult> queryResults = defaultDatabaseMetricsService.getQueryResults();
-
-
-   String logMessages = queryResults.stream()
-           .map(DatabaseMetricResult::toString)
-           .collect(Collectors.joining("\n"));
-   return ResponseEntity.ok(logMessages);
-  } catch (Exception e) {
-   return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-  }
- }
+    @GetMapping("/query")
+    public ResponseEntity<String> executeAndReturnQueryResults() {
+        try {
+            DatabaseCheckMetaData.checkMetaData();
+            List<DatabaseMetricResult> queryResults = defaultDatabaseMetricsService.getQueryResults();
+            String logMessages = queryResults.stream()
+                    .map(DatabaseMetricResult::toString)
+                    .collect(Collectors.joining("\n"));
+            return ResponseEntity.ok(logMessages);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/java/com/dmadev/prometheus/service/DefaultEmployeeService.java
+++ src/main/java/com/dmadev/prometheus/service/DefaultEmployeeService.java
@@ -2,13 +2,12 @@ package com.dmadev.prometheus.service;
 
 import com.dmadev.prometheus.entity.Employee;
 import com.dmadev.prometheus.repository.EmployeeRepository;
+import com.dmadev.prometheus.util.DatabaseCheckMetaData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,13 +17,10 @@ public class DefaultEmployeeService implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-
-
-
     @Override
     @Transactional(readOnly = true)
     public List<Employee> getAllEmployees() {
+        DatabaseCheckMetaData.checkMetaData();
         return employeeRepository.findAll();
     }
-
 }
